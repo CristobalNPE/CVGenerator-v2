6305909e8a78a3b631cb7dd4368c90334af59229
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-12 20:41:16 -0300

    create section

--- src/pages/personalInfo/PersonalInfo.tsx
+++ src/pages/personalInfo/PersonalInfo.tsx
@@ -1,32 +1,24 @@
-import SelectionCards from "@/components/SelectionCards/SelectionCards";
 import Stepper from "@/components/Stepper/Stepper";
 import { Heading } from "@/components/typography/Heading";
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { templates } from "./data";
 
-function TemplateSelection() {
-  const [selectedItem, setSelectedItem] = useState("Serio");
+function PersonalInfo() {
   return (
     <main className="p-4 text-center flex flex-col  items-center w-full  ">
       <Stepper />
-      <Heading variant={"h2"}>Seleccione una plantilla</Heading>
+      <Heading variant={"h2"}>Completa tu información personal</Heading>
 
       <div className="grow">
-        <SelectionCards
-          selectedItem={selectedItem}
-          setSelectedItem={setSelectedItem}
-          cards={templates}
-        />
+       
       </div>
 
       <Link
         className={`mt-5 ${buttonVariants({
           variant: "default",
         })} w-full md:w-fit md:text-md md:py-6 md:px-12`}
-        to={"/personalInfo"}
+        to={"/"}
       >
         {" "}
         <ArrowRight className="mr-2 " strokeWidth={3} /> Siguiente
@@ -35,4 +27,4 @@ function TemplateSelection() {
   );
 }
 
-export default TemplateSelection;
+export default PersonalInfo;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import MainLayout from "@/components/layout/MainLayout";
 import Home from "@/pages/home/Home";
+import PersonalInfo from "@/pages/personalInfo/PersonalInfo";
 import SizeSelection from "@/pages/sizeSelection/SizeSelection";
 import TemplateSelection from "@/pages/templateSelection/TemplateSelection";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -21,6 +22,10 @@ function AppRoutes() {
         {
           path: "template",
           element: <TemplateSelection />,
+        },
+        {
+          path: "personalInfo",
+          element: <PersonalInfo />,
         },
       ],
     },
