61da620516d70e9a5dc4bbc2a0a3448d056eb0f3
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-12 20:36:13 -0300

    clean up

--- src/pages/sizeSelection/SizeSelection.tsx
+++ src/pages/sizeSelection/SizeSelection.tsx
@@ -1,53 +1,32 @@
 import Stepper from "@/components/Stepper/Stepper";
 import { Heading } from "@/components/typography/Heading";
 import { buttonVariants } from "@/components/ui/button";
-import { ArrowRight, Newspaper } from "lucide-react";
+import { ArrowRight } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import SelectionCards from "./components/SelectionCards/SelectionCards";
-import { SelectionCardProps } from "./components/SelectionCards/SelectionCard";
+import SelectionCards from "../../components/SelectionCards/SelectionCards";
+import { cards } from "./data";
 function SizeSelection() {
-  const cards: SelectionCardProps[] = [
-    {
-      title: "A4",
-      description:
-        "Para un currículum profesional estándar con formato internacional, perfecto para oportunidades laborales globales.",
-      recommended: true,
-      subtitle: "210mm x 297mm",
-    },
-    {
-      title: "Carta",
-      description:
-        "Un currículum con un formato que se adapta a las normas de países norteamericanos.",
-      subtitle: "216mm x 279mm",
-    },
-    {
-      title: "Oficio",
-      description:
-        "Formato de hoja larga utilizado comúnmente en países de Sudamérica.",
-      subtitle: "216mm x 330mm",
-    },
-  ];
-
-
-  //Control selected cards here. If none selected, disable next button.
-
+  const [selectedItem, setSelectedItem] = useState("A4");
 
   return (
     <main className="p-4 text-center flex flex-col  items-center w-full  ">
       <Stepper />
-      <Heading variant={"h2"}>Que formatos de papel preparamos?</Heading>
+      <Heading variant={"h2"}>Escoja el formato de papel</Heading>
 
-      <SelectionCards
-        cards={cards}
-        icon={<Newspaper size={48} strokeWidth={1.25} />}
-      />
+      <div className="grow">
+        <SelectionCards
+          selectedItem={selectedItem}
+          setSelectedItem={setSelectedItem}
+          cards={cards}
+        />
+      </div>
 
-      {/* when selected, button continuar */}
       <Link
         className={`mt-5 ${buttonVariants({
           variant: "default",
         })} w-full md:w-fit md:text-md md:py-6 md:px-12`}
-        to={"/"}
+        to={"/template"}
       >
         {" "}
         <ArrowRight className="mr-2 " strokeWidth={3} /> Siguiente
