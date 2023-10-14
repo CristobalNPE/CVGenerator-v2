52434396ad3121b54377b0337f1b460de3f89eb0
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-14 18:17:01 -0300

    fix sizing

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -6,19 +6,19 @@ function Stepper() {
   //outside of outlet???
 
   return (
-    <article className="flex gap-10 mb-10 justify-around items-center w-full sm:max-w-sm">
-      <section className="opacity-40 text-md">
-        <div className="text-2xl font-semibold">0</div>
-        <Badge variant={"outline"}>Inicio</Badge>
+    <article className="flex gap-10 mb-10 justify-between items-center w-full sm:max-w-sm">
+      <section className="text-md cursor-pointer">
+        <div className="text-xl font-semibold">0</div>
+        <Badge variant={"secondary"}>Inicio</Badge>
       </section>
 
       <section className="text-lg ">
-        <div className="text-3xl font-black">1</div>
+        <div className="text-2xl font-black">1</div>
         <Badge>Formato</Badge>
       </section>
 
-      <section className="opacity-40  text-md ">
-        <div className="text-2xl font-semibold">2</div>
+      <section className="opacity-30  text-md ">
+        <div className="text-xl font-semibold">2</div>
         <Badge variant={"outline"}>Plantilla</Badge>
       </section>
     </article>
