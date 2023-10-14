055370009beda86a6317d5e2b9aa7736359511eb
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-14 18:16:39 -0300

    delete comments

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -7,13 +7,13 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <div className=" bg-background text-foreground min-h-[100dvh] flex overflow-hidden">
-      {/* //* Navbar? //* (children) \\ Outlet (wrapped in suspense) with max width */}
-      <ScrollRestoration/>
-      <Suspense fallback={<h1 className="text-red-600">Loading...</h1>}>
+    <div className=" bg-background text-foreground min-h-[100dvh] flex  overflow-hidden">
+      <ScrollRestoration />
+      <Suspense
+        fallback={<h1 className="text-red-600 text-6xl">Loading...</h1>}
+      >
         <Outlet />
       </Suspense>
-      {/* //* Footer? */}
     </div>
   );
 }
