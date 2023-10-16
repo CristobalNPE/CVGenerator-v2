b63b326a14e2d7c66cb322d0930598df3f489176
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-16 14:52:59 -0300

    add NavigationBar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,18 +2,19 @@
 //   children: React.ReactNode;
 // };
 
+import NavigationBar from "@/components/NavigationBar/NavigationBar";
 import { Suspense } from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
-import { Button } from "../ui/button";
-import { Menu } from "lucide-react";
 
 function MainLayout() {
   return (
-    <div className=" bg-background text-foreground min-h-[100dvh] flex  overflow-hidden ">
+    <div className=" bg-background text-foreground min-h-[100dvh] flex flex-col   items-center  overflow-hidden ">
       <ScrollRestoration />
+      <NavigationBar/>
       <Suspense
         fallback={<h1 className="text-red-600 text-6xl">Loading...</h1>}
       >
+        
         <Outlet />
       </Suspense>
       {/* <Button variant={"secondary"} className="rounded-full p-1 h-14 w-14  bottom-0"><Menu size={30} /></Button> */}
