bff443ebcc121ebc31796df773eca23b8834b692
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-16 14:54:21 -0300

    add option to save current cv to localstorage

--- src/pages/result/Result.tsx
+++ src/pages/result/Result.tsx
@@ -4,23 +4,37 @@ import VisualTemplate from "@/components/templates/VisualTemplate";
 import { Heading } from "@/components/typography/Heading";
 import { Button } from "@/components/ui/button";
 import { useCurriculumStore } from "@/stores/curriculum";
+import { Download, Save } from "lucide-react";
 
 function Result() {
   const curriculum = useCurriculumStore();
   //TODO: MAKE THE MENU FOR TOGGLING DARK MODE AND ACCESS SAVED CVs
-  //TODO: FIX STEPPER
   //TODO: MAKE IT SO IT CANT SKIP STEPS
-  //TODO: FOR NOW JUST SHOW THE HTML
   //TODO: ADD ANIMATION BETWEEN PAGES
   //TODO: WHEN LEARN NODE, SEND THE TEMPLATES HTML TO GET A PDF
+
+  const saveCV = () => {
+    const currentDateTime = new Date().toLocaleString();
+    console.log(currentDateTime);
+    curriculum.setCreationDate(currentDateTime);
+    localStorage.setItem(
+      `cvCreator-${curriculum.id}`,
+      JSON.stringify(curriculum)
+    );
+  };
+
   return (
     <main className="p-4 text-center flex flex-col  items-center w-full  ">
-
-
-      <div className="flex flex-col sm:flex-row sm:gap-10 sm:mb-5">
-        <Heading variant={"h2"}>Tu Curriculum!: </Heading>
-        <Button onClick={() => alert("Próximamente! 😞")} variant={"secondary"}>
-          Descargar!
+      <Heading variant={"h2"}>Tu Curriculum: </Heading>
+      <div className="my-2 flex flex-col gap-2 sm:flex-row sm:gap-10 sm:mb-5">
+        <Button onClick={saveCV} variant={"secondary"}>
+          <Save className="mr-2" /> Guardar Datos
+        </Button>
+        <Button
+          onClick={() => alert("Próximamente! 😞😞")}
+          variant={"secondary"}
+        >
+          <Download className="mr-2" /> Descargar CV
         </Button>
       </div>
 
